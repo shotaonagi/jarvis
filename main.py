f27fe5e0f76dba0d117e5124de41f4cfe1b4613b
EC2 Default User <ec2-user@ip-172-31-16-139.ap-southeast-2.compute.internal>
2023-12-23 16:14:44 +0000

    main.py_update

--- main.py
+++ main.py
@@ -1,22 +1,105 @@
-from fastapi import FastAPI, HTTPException, Query, UploadFile, File
+from fastapi import FastAPI, HTTPException, Query, UploadFile, File, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
 from openai import OpenAI, OpenAIError
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-# APIクライアントの設定
-client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+# JWTシークレットキーとアルゴリズム
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# データベース接続とモデル定義
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# データベースモデル
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class Assistant(Base):
+    __tablename__ = "assistants"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    model = Column(String)
+
+class Thread(Base):
+    __tablename__ = "threads"
+    id = Column(Integer, primary_key=True, index=True)
+    assistant_id = Column(Integer, ForeignKey('assistants.id'))
+    assistant = relationship("Assistant")
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True, index=True)
+    thread_id = Column(Integer, ForeignKey('threads.id'))
+    thread = relationship("Thread")
+    content = Column(String)
+    role = Column(String)
+
+class File(Base):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True, index=True)
+    file_content = Column(LargeBinary)
+    purpose = Column(String)
+
+# FastAPIアプリケーションの初期化
 app = FastAPI()
 
 # CORSミドルウェアの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # すべてのオリジンを許可する場合
+    allow_origins=["*"],  # すべてのオリジンを許可
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# APIクライアントの設定
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+# 依存関係
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# パスワードをハッシュ化する関数
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# パスワードの検証
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# トークンの生成
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 # モデル定義
 class CreateAssistantRequest(BaseModel):
@@ -37,39 +120,58 @@ class RunAssistantRequest(BaseModel):
     thread_id: str
     assistant_id: str
 
-# エンドポイント定義
-@app.post("/create_assistant")
-def create_assistant(request: CreateAssistantRequest):
-    try:
-        assistant = client.beta.assistants.create(
-            name=request.name,
-            instructions=request.instructions,
-            model=request.model,
-            tools=request.tools
+# ユーザー作成のエンドポイント
+@app.post("/users/")
+def create_user(username: str, email: str, password: str, db: Session = Depends(get_db)):
+    hashed_password = hash_password(password)
+    db_user = User(username=username, email=email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
         )
-        return {"assistant_id": assistant.id}
-    except OpenAIError as e:
-        raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+@app.post("/create_assistant")
+def create_assistant(request: CreateAssistantRequest, db: Session = Depends(get_db)):
+    assistant = Assistant(name=request.name, model=request.model)
+    db.add(assistant)
+    db.commit()
+    db.refresh(assistant)
+    return {"assistant_id": assistant.id}
 
 @app.post("/create_thread")
-def create_thread(request: CreateThreadRequest):
-    try:
-        thread = client.beta.threads.create()
-        return {"thread_id": thread.id}
-    except OpenAIError as e:
-        raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
+def create_thread(db: Session = Depends(get_db)):
+    thread = Thread()
+    db.add(thread)
+    db.commit()
+    db.refresh(thread)
+    return {"thread_id": thread.id}
 
 @app.post("/add_message")
-def add_message(request: AddMessageRequest):
-    try:
-        message = client.beta.threads.messages.create(
-            thread_id=request.thread_id,
-            role=request.role,
-            content=request.content
-        )
-        return {"message_id": message.id}
-    except OpenAIError as e:
-        raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
+def add_message(request: AddMessageRequest, db: Session = Depends(get_db)):
+    message = Message(thread_id=request.thread_id, content=request.content, role=request.role)
+    db.add(message)
+    db.commit()
+    db.refresh(message)
+    return {"message_id": message.id}
 
 @app.post("/run_assistant")
 def run_assistant(request: RunAssistantRequest):
@@ -94,24 +196,15 @@ def check_run_status(thread_id: str = Query(...), run_id: str = Query(...)):
         raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
 
 @app.get("/get_responses")
-def get_responses(thread_id: str = Query(...)):
-    try:
-        messages = client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
-        responses = [message.content for message in messages.data if message.role == "assistant"]
-        return {"responses": responses}
-    except OpenAIError as e:
-        raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
+def get_responses(thread_id: str = Query(...), db: Session = Depends(get_db)):
+    responses = db.query(Message).filter(Message.thread_id == thread_id, Message.role == "assistant").all()
+    return {"responses": [response.content for response in responses]}
 
 @app.post("/upload_file")
-async def upload_file(file: UploadFile = File(...)):
-    try:
-        file_content = await file.read()
-        uploaded_file = client.files.create(
-            file=file_content,
-            purpose='assistants'
-        )
-        return {"file_id": uploaded_file.id}
-    except OpenAIError as e:
-        raise HTTPException(status_code=500, detail=f"OpenAI API error: {e}")
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_content = await file.read()
+    db_file = File(file_content=file_content, purpose='assistants')
+    db.add(db_file)
+    db.commit()
+    db.refresh(db_file)
+    return {"file_id": db_file.id}
